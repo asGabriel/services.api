8e35ad396890477cec84d178dffe4546f8882fe6
asGabriel <andrade.gabriel2295@gmail.com>
2024-06-05 07:57:05 -0300

    refac(budget): update at transactions table fields

---
+++ migrations/20240605104259_refac-transactions-table.sql
@@ -0,0 +1,14 @@
+ALTER TABLE
+    transactions DROP COLUMN recurring;
+
+ALTER TABLE
+    transactions DROP COLUMN recurrence_frequency;
+
+ALTER TABLE
+    transactions DROP COLUMN note;
+
+ALTER TABLE
+    transactions DROP COLUMN month_reference;
+
+ALTER TABLE
+    transactions DROP COLUMN year_reference;
